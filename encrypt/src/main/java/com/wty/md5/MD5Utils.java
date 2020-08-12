a2353b684abb9836542e3a38c3dcdcf8f12cfffd
RitualYang <995022373@qq.com>
2020-08-12 23:37:20 +0800

    netty-websocket

---
+++ encrypt/src/main/java/com/wty/md5/MD5Utils.java
@@ -0,0 +1,42 @@
+package com.wty.md5;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+/**
+ * @author WTY
+ * @Date 2020/7/29 22:00
+ */
+public class MD5Utils {
+    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        System.out.println(encodeByMd5("73.25"));
+        System.out.println(MD5("73.25"));
+    }
+
+    public static String MD5(String string) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Huh, MD5 should be supported?", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Huh, UTF-8 should be supported?", e);
+        }
+
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) hex.append("0");
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString();
+    }
+    public static String encodeByMd5(String string) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        // 确定计算方法
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        Base64.Encoder base64Encoder = Base64.getEncoder();
+        // 加密字符串
+        return base64Encoder.encodeToString(md5.digest(string.getBytes("utf-8")));
+    }
+}
